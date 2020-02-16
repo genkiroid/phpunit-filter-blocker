58678cbfcb02fd206d99d1c568ee45ef2eaceb5c
genkiroid <gamurai@gmail.com>
2020-02-16 23:19:10 +0900

    Add test

---
+++ tests/ListenerTest.php
@@ -0,0 +1,23 @@
+<?php
+use PHPUnitFilterBlocker\Listener;
+
+class ListenerTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $listener = new Listener();
+
+        $this->assertTrue($listener->hasBeenSpecifiedTestCase());
+        $this->assertFalse($listener->blockGroup());
+        $this->assertFalse($listener->blockExcludeGroup());
+    }
+
+    public function testConstructWithArgs()
+    {
+        $listener = new Listener(['blockGroup' => true, 'blockExcludeGroup' => true]);
+
+        $this->assertTrue($listener->hasBeenSpecifiedTestCase());
+        $this->assertTrue($listener->blockGroup());
+        $this->assertTrue($listener->blockExcludeGroup());
+    }
+}
